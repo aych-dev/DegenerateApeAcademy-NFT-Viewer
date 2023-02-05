9f36159e5e32c396f9aadb0f85529268d8c47fdb
aych-dev <aychdev1@gmail.com>
2023-02-05 16:50:44 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import AcademyImage from './Images/DegenerateApeAcademyLogo.png';
+import styled from 'styled-components';
+import { Card, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+export default function App() {
+  const [apeData, setApeData] = useState([]);
+  const [apeId, setApeId] = useState('TAP TO BEGIN');
+  const [apeImage, setApeImage] = useState(AcademyImage);
+
+  useEffect(() => {
+    const getMetadata = async () => {
+      const { data } = await axios.get('https://aychserver.herokuapp.com/api');
+      setApeData(data);
+    };
+    getMetadata();
+  }, []);
+
+  // Selects random index of the apeData array
+  const randomIndex = () => {
+    return Math.floor(Math.random() * apeData.length);
+  };
+
+  // Generates random ape image and ape ID
+  const handleClick = () => {
+    const randomApe = apeData[randomIndex()];
+    setApeImage(randomApe.offChainData.image);
+    setApeId(randomApe.offChainData.name);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <Container>
+        <MainCard text='white' bg='dark'>
+          <CardImg
+            onClick={handleClick}
+            src={apeImage}
+            alt='Degenerate Ape Academy'
+          />
+          <Card.Body>
+            <Card.Title className='text-center' as='h2'>
+              {apeId}
+            </Card.Title>
+          </Card.Body>
+        </MainCard>
+      </Container>
+    </MainContainer>
   );
 }
 
-export default App;
+const MainContainer = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #28282b;
+`;
+
+const CardImg = styled(Card.Img)`
+  height: 500px;
+  weight: 500px;
+  border-radius: 30px;
+  cursor: pointer;
+  padding: 10px;
+
+  @media only screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) {
+    height: 385px;
+    width: 385px;
+  }
+`;
+
+const MainCard = styled(Card)`
+  border-radius: 20px;
+  font-family: 'Bangers', 'cursive';
+
+  @media only screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    width: 390;
+  }
+`;
